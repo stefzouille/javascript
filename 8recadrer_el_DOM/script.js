7f4c8bab1ab736deea61ab0b2163596e38882f56
stefzouille <contact.lokcy@gmail.com>
2021-10-19 15:46:13 +0200

    ad

--- 8recadrer_el_DOM/script.js
+++ 8recadrer_el_DOM/script.js
@@ -1,7 +1,41 @@
 const main = document.querySelector('.main');
 
 
-document.addEventListener('mousedown', (e => {
+document.addEventListener('mousedown', (e) => {
+  //set moussemoove x + securité pour la mousse sur le dom
+  if (e.target.className == "drag-x" && (window.event != null)) {
 
-  if (e.target.value)
-}))
+    document.onmousemove = (e) => {
+      e == null ? e = window.event : "";
+      console.log(e.clientX + '////' + main.clientWidth);
+      document.querySelector(".box-1").style.flex = "0 0" + e.clientX / (main.clientWidth / 100) + "%";
+    }
+    preventBahavior(e);
+  }
+  //set moussemoove y + securité pour la mousse sur le dom
+  else if (e.target.className == "drag-y" && (window.event != null)) {
+
+    document.onmousemove = (e) => {
+      e == null ? e = window.event : "";
+      console.log(e.clientY + '////' + main.clientWidth);
+      document.querySelector(".top").style.flex = "0 0" + e.clientY / (main.clientHeight / 100) + "%";
+    }
+    preventBahavior(e);
+  }
+});
+
+//stop mouvement mousse up:
+document.addEventListener('mouseup', (e) => {
+  if (e.target != null) {
+    document.onmousemove = null;
+    document.onselectstart = null;
+    e.target.ondragstart = null;
+  }
+})
+
+//prevent bad behavior on moving element:
+const preventBahavior = (e) => {
+  document.onselectstart = () => false;
+  e.target.ondragstart = () => false;
+  return false;
+}
